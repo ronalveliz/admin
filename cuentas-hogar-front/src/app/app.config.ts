f7b3c67e0e0fd30a576d48b9ffcc3e06a9ed76ff
RONAL VELIZ <148784629+ronalveliz@users.noreply.github.com>
2026-06-09 00:28:40 +0200

    se incorpora componente home

--- cuentas-hogar-front/src/app/app.config.ts
+++ cuentas-hogar-front/src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
+    provideHttpClient( withFetch()),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
--- cuentas-hogar-front/src/app/app.routes.ts
+++ cuentas-hogar-front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { UsuariosForm } from './usuarios-form/usuarios-form';
+import { Home } from './home/home';
 
 
 export const routes: Routes = [
@@ -13,10 +14,15 @@ export const routes: Routes = [
     path: 'login',
     component: Login
   },
-{
+  {
     path: 'register',
     component: UsuariosForm
-}
+  },
+  {
+    path: 'home',
+    component: Home
+  }
+
 
 
 ];
